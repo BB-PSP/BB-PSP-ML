4b827cb1d827f3b2ed78931ad4799e6a94b448e7
minkyu <mink91j@gmail.com>
2022-05-15 19:59:40 +0900

    modified features

--- PITCHER_BACKPROP/PITCHER_BACKPROP_ERA.py
+++ PITCHER_BACKPROP/PITCHER_BACKPROP_ERA.py
@@ -13,9 +13,9 @@ pd.set_option('display.max_seq_items', None)                                    
 mlb_pitchingstats = pd.read_csv(filepath_or_buffer="./DATA/PITCHERS_TOTAL_MLB.csv",
                         encoding="utf_8",sep=",")
 
-pitching_info = mlb_pitchingstats[['Age','W','L','ERA','G','GS','CG','ShO','SV','BS','HLD','IP','TBF','H','R','ER','HR','BB','IBB','HBP','SO','WHIP','BABIP','LOB_PCT','FIP']]
+pitching_info = mlb_pitchingstats[['Age','W','L','ERA','G','GS','CG','ShO','SV','BS','HLD','IP','TBF','H','R','HR','BB','IBB','HBP','SO','WHIP','BABIP','LOB_PCT']]
 
-b_x = pitching_info[['Age','W','L','G','GS','CG','ShO','SV','BS','HLD','IP','TBF','H','R','ER','HR','BB','IBB','HBP','SO','WHIP','BABIP','LOB_PCT','FIP']]
+b_x = pitching_info[['Age','W','L','G','GS','CG','ShO','SV','BS','HLD','IP','TBF','H','R','HR','BB','IBB','HBP','SO','WHIP','BABIP','LOB_PCT']]
 b_y = pitching_info[['ERA']]
 
 b_x_train, b_x_test, b_y_train, b_y_test = train_test_split(b_x, b_y, train_size=0.8,test_size=0.2)
@@ -38,7 +38,7 @@ b_weights = b_model.coef_                                                       
 
 
 b_csv = pd.read_csv("./DATA/PITCHERS_TOTAL_KBO.csv")                                                              #KBO 타자 데이터
-b_kbo_x = b_csv[['Age','W','L','G','GS','CG','ShO','SV','BS','HLD','IP','TBF','H','R','ER','HR','BB','IBB','HBP','SO','WHIP','BABIP','LOB_PCT','FIP']]
+b_kbo_x = b_csv[['Age','W','L','G','GS','CG','ShO','SV','BS','HLD','IP','TBF','H','R','HR','BB','IBB','HBP','SO','WHIP','BABIP','LOB_PCT']]
 b_kbo_y = b_csv[['ERA']]
 
 b_kbo_predict = b_model.predict(b_kbo_x)
